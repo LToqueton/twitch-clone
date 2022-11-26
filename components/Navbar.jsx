00b253ce64573c7a6475377b1126593def90bf5e
LToqueton <leonardo.toqueton@outlook.com>
2022-11-25 23:06:23 -0300

    Layout

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='fixed h-14 w-full flex flex-nowarp items-center p-4 bg-[#7777fd] mb-[2px] z-10'>
+    
+    {/* Left side */}
+    
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,6 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <h1>Hello
-        
-      </h1>
     </div>
   )
 }
