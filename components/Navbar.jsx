4c7dd539b5bd4e90629449acca3176836810cbe7
LToqueton <leonardo.toqueton@outlook.com>
2022-11-30 16:08:30 -0300

    Alter

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,29 +1,34 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import React from 'react'
-import Logo from '../public/assets/logo.png'
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import Logo from "../public/assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div className='fixed h-14 w-full flex flex-nowarp items-center p-4 bg-[#7777fd] mb-[2px] z-10'>
-    
-    {/* Left side */}
-    <div className="flex grow items-center justify-start">
-      <Link href='/'>
+    <div className="fixed h-14 w-full flex flex-nowarp items-center p-4 bg-[#7777fd] mb-[2px] z-10">
+      
+      {/* Left side */}
+      <div className="flex grow items-center justify-start">
+        
+        <Link legacyBehavior href="/">
+          <a className="flex">
+            <Image
+            src={Logo}
+            alt="/"
+            width="36"
+            height="36"
+            className="cursor-pointer z-10"
+          />
+          </a>
+        </Link>
+        
+      </div>
 
-        <Image src={Logo} alt='/' width='36' height='36' className='cursor-pointer'/>
+      {/* Middle */}
 
-      </Link>
-    </div>
-
-    {/* Middle */}
-
-
-    {/* Right side */}
-
-    
+      {/* Right side */}
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
