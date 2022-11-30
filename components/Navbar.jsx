caacfb9ee9574ca2b2dde850e18fd2e9500d33c1
LToqueton <leonardo.toqueton@outlook.com>
2022-11-30 14:28:26 -0300

    Alter config for old projects

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,29 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
+import Logo from '../public/assets/logo.png'
 
 const Navbar = () => {
   return (
     <div className='fixed h-14 w-full flex flex-nowarp items-center p-4 bg-[#7777fd] mb-[2px] z-10'>
     
     {/* Left side */}
+    <div className="flex grow items-center justify-start">
+      <Link href='/'>
+
+        <Image src={Logo} alt='/' width='36' height='36' className='cursor-pointer'/>
+
+      </Link>
+    </div>
+
+    {/* Middle */}
+
+
+    {/* Right side */}
+
     
     </div>
-  )
+  );
 }
 
 export default Navbar
